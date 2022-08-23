2188589b7587b6175ebfa326ec4c0bc3b9e3e81d
yaana <ya.anastasya.b@yandex.ru>
2022-08-23 20:05:31 +0300

    feat: обновление профиля, обновление аватара

--- controllers/users.js
+++ controllers/users.js
@@ -10,12 +10,7 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({
-      // name: user.name,
-      // about: user.about,
-      // avatar: user.avatar,
-      data: user,
-     }))
+    .then((user) => res.send({ data: user }))
     .catch(() => res.status(400).send({ message: 'Переданы некорректные данные' }));
 };
 
@@ -23,4 +18,35 @@ module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => res.send({ data: user }))
     .catch(() => res.status(404).send({ message: 'Пользователь не найден' }));
+};
+
+module.exports.updateUser = (req, res) => {
+  User.findByIdAndUpdate(
+    req.user._id,
+    {
+      name: req.body.name,
+      about: req.body.about,
+    },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(404).send({ message: 'Переданы некорректные данные' }));
+};
+
+module.exports.updateAvatar = (req, res) => {
+  User.findByIdAndUpdate(
+    req.user._id,
+    {
+      avatar: req.body.avatar,
+    },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(404).send({ message: 'Переданы некорректные данные' }));
 };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
-const { getUsers, createUser, getUser } = require('../controllers/users');
+const {
+  getUsers, createUser, getUser, updateUser, updateAvatar,
+} = require('../controllers/users');
 
 router.get('/', getUsers);
 router.post('/', createUser);
 router.get('/:userId', getUser);
+router.patch('/me', updateUser);
+router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
