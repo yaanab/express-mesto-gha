9edab422f3dc5dd1ca860ef934572b37a27a714b
yaana <ya.anastasya.b@yandex.ru>
2022-09-06 19:48:45 +0300

    feat: валидация приходящих запросов

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,27 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getUser, updateUser, updateAvatar, getCurrentUser
 } = require('../controllers/users');
+const { regex } = require('../utils/utils');
 
 router.get('/', getUsers);
-router.get('/:userId', getUser);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
 router.get('/me', getCurrentUser);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24),
+  }),
+}), getUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }).unknown(true),
+}), updateUser);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(regex),
+  }).unknown(true),
+}), updateAvatar);
 
 module.exports = router;
