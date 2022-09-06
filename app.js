269b56b1ba4a06916f52e879600af4b4c1e5ad19
yaana <ya.anastasya.b@yandex.ru>
2022-09-06 09:57:51 +0300

    feat: добавлен auth

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -17,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/signup', createUser);
 app.post('/signin', login);
+app.use(auth);
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 app.use('/', (req, res) => res.status(404).send({ message: 'Запрос не найден' }));
